7874874fafcae5b3ddb1872d2b66446cb8fe44d1
Nelson Gonzalez <enelmgm@gmail.com>
2022-05-03 21:28:51 -0400

    adding github page feature

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+const isProd = process.env.NODE_ENV !== 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
+  assetPrefix: isProd ? '/forecast-weather-nextjs/' : '',
 }
 
 module.exports = nextConfig
