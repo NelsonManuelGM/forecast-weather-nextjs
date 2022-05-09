ddae477ea1a08b5ec708f42c68589edf7c09579d
Nelson Gonzalez <enelmgm@gmail.com>
2022-05-09 19:00:24 -0400

    add icons and configuration

--- components/weather-box/index.js
+++ components/weather-box/index.js
@@ -1,17 +1,33 @@
 import { useTheme } from "styled-components";
 
-import { Wrapper, Header, Title, DensityIco, MoreIco } from "./styles";
+import {
+  Wrapper,
+  Header,
+  Title,
+  IconName,
+  TitleWrapper,
+  Loading,
+} from "./styles";
+import { DensityIco, MoreIco, PersonPinCircleIco, DotIco } from "../icons";
 
 const WeatherBox = ({ place }) => {
-
-  const themeContext = useTheme()
+  const themeContext = useTheme();
 
   return (
     <Wrapper>
       <Header>
         <DensityIco fill={themeContext.palette.platinum} />
-        <Title>{place}</Title>
-        <MoreIco fill={themeContext.palette.platinum}/>
+        <TitleWrapper>
+          <IconName>
+            <PersonPinCircleIco fill={themeContext.palette.platinum} />
+            <Title>{place}</Title>
+          </IconName>
+          <Loading>
+            <DotIco fill={'green'}/>
+            Updating
+          </Loading>
+        </TitleWrapper>
+        <MoreIco fill={themeContext.palette.platinum} />
       </Header>
     </Wrapper>
   );
--- components/weather-box/styles.js
+++ components/weather-box/styles.js
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 export const Title = styled.h1`
   color: ${(props) => props.theme.palette.platinum};
+  margin: 0px;
+  font-size: x-large;
+  font-weight: 500;
 `;
 
 export const Wrapper = styled.section`
@@ -9,7 +12,7 @@ export const Wrapper = styled.section`
   width: 100%;
   height: 100%;
   border-radius: 42px;
-  padding: 10px 20px;
+  padding: 20px 20px;
 
   @media (max-width: ${(props) => props.theme.breakPoint}) {
     border-radius: 0px;
@@ -23,18 +26,30 @@ export const Wrapper = styled.section`
 export const Header = styled.header`
   width: 100%;
   display: flex;
-  align-items: center;
   justify-content: space-between;
 `;
 
-export const DensityIco = ({ title, titleId, ...props }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" height={24} width={24} {...props}>
-    <path d="M13 11V5h2v2.6L19.575 3 21 4.425 16.4 9H19v2ZM4.425 21 3 19.575 7.6 15H5v-2h6v6H9v-2.6Z" />
-  </svg>
-);
+export const IconName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
 
-export const MoreIco = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" height={24} width={24} {...props}>
-    <path d="M12 20q-.825 0-1.412-.587Q10 18.825 10 18q0-.825.588-1.413Q11.175 16 12 16t1.413.587Q14 17.175 14 18q0 .825-.587 1.413Q12.825 20 12 20Zm0-6q-.825 0-1.412-.588Q10 12.825 10 12t.588-1.413Q11.175 10 12 10t1.413.587Q14 11.175 14 12q0 .825-.587 1.412Q12.825 14 12 14Zm0-6q-.825 0-1.412-.588Q10 6.825 10 6t.588-1.412Q11.175 4 12 4t1.413.588Q14 5.175 14 6t-.587 1.412Q12.825 8 12 8Z" />
-  </svg>
-);
+export const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Loading = styled.div`
+  margin-top: 10px;
+  padding: 2px 12px 2px 4px;
+  border: 1px solid ${(props) => props.theme.palette.platinum};
+  border-radius: 50px;
+  font-size: x-small;
+  color: ${(props) => props.theme.palette.platinum};
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
