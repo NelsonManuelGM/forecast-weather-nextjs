3991dedcd58df91ff9793ed046eb9dc1e051054b
Nelson Gonzalez <enelmgm@gmail.com>
2022-10-10 17:57:46 -0400

    update next configuration

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-  assetPrefix: isProd ? '/forecast-weather-nextjs/' : '',
 }
 
 module.exports = nextConfig
